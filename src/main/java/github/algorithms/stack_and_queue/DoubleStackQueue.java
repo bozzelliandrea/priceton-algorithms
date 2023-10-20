ba24fb29e8970c73d9ecf911c36d1452447828fc
bozzelliandrea <bozzelliandrea@yahoo.it>
2023-10-20 18:55:52 +0200

    stack based queue: Queue with two stacks. Implement a queue with two stacks so that each queue operations takes a constant amortized number of stack operations.

---
+++ src/main/java/github/algorithms/stack_and_queue/DoubleStackQueue.java
@@ -0,0 +1,66 @@
+package github.algorithms.stack_and_queue;
+
+
+import java.io.Serializable;
+import java.util.NoSuchElementException;
+
+//double stack based queue
+public class DoubleStackQueue<T extends Serializable> implements Queue<T> {
+
+    private final transient Stack<T> inbox;
+    private final transient Stack<T> outbox;
+
+    public DoubleStackQueue() {
+        inbox = new LinkedStack<>();
+        outbox = new LinkedStack<>();
+    }
+
+    @Override
+    public int size() {
+        return inbox.size() + outbox.size();
+    }
+
+    @Override
+    public void enqueue(T item) {
+        inbox.push(item);
+    }
+
+    @Override
+    public T dequeue() {
+        _validate_and_reverse();
+
+        return outbox.pop();
+    }
+
+    @Override
+    public T peek() {
+        _validate_and_reverse();
+
+        return outbox.peek();
+    }
+
+    @SuppressWarnings("unchecked")
+    public T[] toArray() {
+        int size = size();
+
+        T[] arr = (T[]) new Serializable[size];
+
+        for (int i = 0; i < size; i++)
+            arr[i] = this.dequeue();
+
+        for (int i = 0; i < size; i++) {
+            this.enqueue(arr[i]);
+        }
+
+        return arr;
+    }
+
+    private void _validate_and_reverse() {
+        if (inbox.isEmpty() && outbox.isEmpty())
+            throw new NoSuchElementException("Queue underflow");
+
+        if (!inbox.isEmpty() && outbox.isEmpty())
+            while (!inbox.isEmpty())
+                outbox.push(inbox.pop());
+    }
+}
---
+++ src/test/java/github/algorithms/data_structure/DoubleStackQueueTest.java
@@ -0,0 +1,62 @@
+package github.algorithms.data_structure;
+
+import github.algorithms.stack_and_queue.DoubleStackQueue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.Arrays;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DoubleStackQueueTest {
+
+    private static final Integer value1 = 1;
+    private static final Integer value2 = 2;
+    private static final Integer value3 = 3;
+
+    private static final DoubleStackQueue<Integer> queue = new DoubleStackQueue<>();
+
+    @Test
+    @Order(1)
+    void loadQueue() {
+        Assertions.assertTrue(queue.isEmpty());
+
+        queue.enqueue(value1);
+        Assertions.assertEquals(value1, queue.peek());
+
+        queue.enqueue(value2);
+        Assertions.assertEquals(value1, queue.peek());
+
+        queue.enqueue(value3);
+        Assertions.assertEquals(value1, queue.peek());
+
+        Assertions.assertFalse(queue.isEmpty());
+        Assertions.assertEquals(3, queue.size());
+
+        Assertions.assertEquals(
+                Arrays.toString(new Integer[]{1, 2, 3}),
+                Arrays.toString(queue.toArray())
+        );
+    }
+
+    @Test
+    @Order(1)
+    void unloadQueue() {
+        Assertions.assertFalse(queue.isEmpty());
+        Assertions.assertEquals(3, queue.size());
+
+        Assertions.assertEquals(value1, queue.dequeue());
+
+        Assertions.assertEquals(value2, queue.peek());
+        Assertions.assertEquals(value2, queue.dequeue());
+
+        Assertions.assertEquals(value3, queue.peek());
+        Assertions.assertFalse(queue.isEmpty());
+        Assertions.assertEquals(value3, queue.dequeue());
+
+        Assertions.assertTrue(queue.isEmpty());
+        Assertions.assertEquals(0, queue.size());
+    }
+}
